bfe08f56063caaac6f6a674942123569d6bb1704
jestar <jestar719@hotmail.com>
2016-10-14 11:52:10 +0800

    更新官方样例3

--- app/src/main/java/cn/jestar/coodinatorlayoutdemo/AppConfig.java
+++ app/src/main/java/cn/jestar/coodinatorlayoutdemo/AppConfig.java
@@ -7,5 +7,7 @@ public interface AppConfig {
     Class<? extends BaseActivity>[] ACTIVITYS = new Class[]{
             OfficialSample1Activity.class,
             OfficialSample1ExtActivity.class,
-            OfficialSample2Activity.class};
+            OfficialSample2Activity.class,
+            OfficialSample3Activity.class
+    };
 }
--- app/src/main/java/cn/jestar/coodinatorlayoutdemo/OfficialSample3Activity.java
+++ app/src/main/java/cn/jestar/coodinatorlayoutdemo/OfficialSample3Activity.java
@@ -1,10 +1,10 @@
 package cn.jestar.coodinatorlayoutdemo;
 
 /**
- * 官方样例2,CollapsingToolbarLayout的初步使用
+ * 官方样例3,视差模式
  * Created by jestar on 16/10/13.
  */
-public class OfficialSample2Activity  extends BaseToolbarActivity{
+public class OfficialSample3Activity extends BaseToolbarActivity{
     @Override
     protected void init() {
         super.init();
@@ -13,6 +13,6 @@ public class OfficialSample2Activity  extends BaseToolbarActivity{
 
     @Override
     protected int getLayoutId() {
-        return R.layout.activity_official_sample2;
+        return R.layout.activity_official_sample3;
     }
 }
