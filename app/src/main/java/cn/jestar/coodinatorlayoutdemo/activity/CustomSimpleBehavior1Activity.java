702bf5b9887d21e0ba6bea35100cced1284be9d5
jestar <jestar719@hotmail.com>
2016-10-16 22:49:41 +0800

    修改了自定义简单演示1
    新增一个有dependency,演示一对多的关系

--- app/src/main/java/cn/jestar/coodinatorlayoutdemo/activity/CustomSimpleBehavior1Activity.java
+++ app/src/main/java/cn/jestar/coodinatorlayoutdemo/activity/CustomSimpleBehavior1Activity.java
@@ -2,33 +2,31 @@ package cn.jestar.coodinatorlayoutdemo.activity;
 
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
 
-import butterknife.BindView;
+import butterknife.OnTouch;
 import cn.jestar.coodinatorlayoutdemo.R;
 
 /**
  * 自定义简单的Behavior演示
  * 演示了在View上使用注解添加Behavior
+ * 演示了Behavior的一对多的关系
  * Created by jestar on 16/10/14.
  */
 
 public class CustomSimpleBehavior1Activity extends BaseCustomTitleActivity {
-    @BindView(R.id.tv_dependency)
-    TextView mTvDependency;
+
+    @OnTouch({R.id.tv_dependency_a,R.id.tv_dependency_b})
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+            view.setX(motionEvent.getRawX() - view.getWidth() / 2);
+            view.setY(motionEvent.getRawY() - view.getHeight() / 2);
+        }
+        return false;
+    }
 
     @Override
     protected void init() {
-        mTvDependency.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                    view.setX(motionEvent.getRawX() - view.getWidth() / 2);
-                    view.setY(motionEvent.getRawY() - view.getHeight() / 2);
-                }
-                return false;
-            }
-        });
+
     }
 
     @Override
